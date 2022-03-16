2c57f32c9921d98001d8379be70ae92e37576426
Eungwang <eungwang1203@gmail.com>
2022-03-17 05:16:28 +0900

    데이터 받아서 JSON파일로 추출하기 성공

--- ApiFactory/src/app.js
+++ ApiFactory/src/app.js
@@ -1,15 +1,18 @@
 const axios = require("axios");
+const utf8 = require("utf8");
 const { generateHmac } = require("./lib/hmacGenerator");
 const fs = require("fs");
 
-const REQUEST_METHOD = "POST";
+const keyword = "노트북";
+const convertKeyword = encodeURIComponent(keyword);
+
+const REQUEST_METHOD = "GET";
 const DOMAIN = "https://api-gateway.coupang.com";
-const URL = "/v2/providers/affiliate_open_api/apis/openapi/v1/deeplink";
+const URL = `/v2/providers/affiliate_open_api/apis/openapi/products/search?keyword=${convertKeyword}&limit=10&subid=eungwang1203`;
 
 // Replace with your own ACCESS_KEY and SECRET_KEY
 const ACCESS_KEY = "8ffaeca8-2c9d-4e68-a208-2ac6d728fc51";
 const SECRET_KEY = "e6b0e1c0e5589ff34061c21fc338e3ccd1ba954a";
-
 const REQUEST = {
   coupangUrls: [
     "https://www.coupang.com/np/search?component=&q=good&channel=user",
@@ -17,21 +20,25 @@ const REQUEST = {
   ],
 };
 
-(async () => {
+const generateData = async () => {
   const authorization = generateHmac(REQUEST_METHOD, URL, SECRET_KEY, ACCESS_KEY);
   axios.defaults.baseURL = DOMAIN;
 
   try {
     const response = await axios.request({
+      headers: { Authorization: authorization },
       method: REQUEST_METHOD,
       url: URL,
-      headers: { Authorization: authorization },
-      data: REQUEST,
     });
-    console.log(response.data);
-    const testJSON = JSON.stringify(response.data);
-    fs.writeFileSync("test.json", testJSON);
+    console.log(response.data.data.productData);
+    fs.writeFile(`./${keyword}.json`, JSON.stringify(response.data.data.productData, null, 4), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("File has been created");
+    });
   } catch (err) {
     console.error(err.response.data);
   }
-})();
+};
