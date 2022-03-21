7ce1061bf39c36a5613e5fc2336cf901ca2b1825
Eungwang <eungwang1203@gmail.com>
2022-03-21 15:56:51 +0900

    ADD ScrollToTopBtn

--- coupang/src/component/AppLayout.tsx
+++ coupang/src/component/AppLayout.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { useRecoilValue } from 'recoil';
 import { indexStateAtom } from 'src/Atoms/atom';
 import SearchInput from './SearchInput';
+import { useEffect, useRef, useState } from 'react';
 
 interface Props {
   children: React.ReactNode;
@@ -13,6 +14,26 @@ const TestLayout: React.FunctionComponent<Props> = ({ children, keywords }) => {
   const items: { value: string }[] = [];
   keywords?.map((el) => items.push({ value: el }));
   const indexState = useRecoilValue(indexStateAtom);
+  const [scrollToTopBtnState, setScrollToTopBtnState] = useState(false);
+  const scrollBtnRef = useRef<null | HTMLDivElement>(null);
+
+  useEffect(() => {
+    const scrollFunc = () => {
+      if (window.scrollY >= 100) {
+        setScrollToTopBtnState(true);
+      } else {
+        setScrollToTopBtnState(false);
+      }
+    };
+    window.addEventListener('scroll', scrollFunc);
+  }, []);
+
+  const scrollToTOP = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
 
   return (
     <Layout>
@@ -39,11 +60,20 @@ const TestLayout: React.FunctionComponent<Props> = ({ children, keywords }) => {
       </Header>
       {children}
       <Footer>Copyright © 2022 All Rights Reserved by Eungwang</Footer>
+      {scrollToTopBtnState && (
+        <ScrollToTopBtn scrollToTopBtnState onClick={scrollToTOP} ref={scrollBtnRef}>
+          ▲
+        </ScrollToTopBtn>
+      )}
     </Layout>
   );
 };
 
 export default TestLayout;
+
+interface IScrollToTopBtn {
+  scrollToTopBtnState: boolean;
+}
 
 const LogoWrapper = styled.div`
   width: 40%;
@@ -85,4 +115,23 @@ const IframeWrapper = styled.div`
   @media screen and (max-width: 992px) {
     width: 90%;
   }
+`;
+
+const ScrollToTopBtn = styled.div<IScrollToTopBtn>`
+  position: fixed;
+  border-radius: 10px;
+  padding: 10px 20px;
+  transition-property: opacity;
+  transition-duration: 1s;
+  background-color: #ffa500;
+  bottom: 15px;
+  right: 15px;
+  z-index: 119;
+  color: white;
+  cursor: pointer;
+  :hover {
+    background-color: #f5aa20;
+  }
+  display: ${(props) => (props.scrollToTopBtnState ? 'block' : 'none')};
+  opacity: ${(props) => (props.scrollToTopBtnState ? '1' : '0')};
 `;
