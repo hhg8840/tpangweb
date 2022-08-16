17a2e5e8e8de846fde51e3b888963e988533c647
getup9 <getup9@gmail.com>
2022-08-17 08:37:23 +0900

    20220817

---
+++ coupangapi/autopostkeyword.py
@@ -0,0 +1,52 @@
+from coupang import cupang_keyword_search, get_cuplink
+import os
+import json
+import datetime
+now_path = os.path.dirname(os.path.abspath(__file__))
+# file = open(f'{now_path}/metadata/setting.json',
+#             'r', encoding='utf-8')
+# config_dict = json.load(file)
+
+
+def main():
+
+    keyword = '웹캠'
+    files_Path = f"{now_path}/product_posts/"
+
+    limit = 10
+    product_list = cupang_keyword_search(keyword, limit)
+
+    print(product_list)
+
+    # TRANSLATE LINK
+    originUrlList  = []
+    for product in product_list:
+        productId = product['productId']
+
+        transUrl = f'https://www.coupang.com/vp/products/{productId}'
+        originUrlList.append(transUrl)
+
+    cupResult = get_cuplink(originUrlList)
+
+    for idx,product in enumerate(product_list):
+        product['productUrl'] = cupResult[idx]['shortenUrl']
+
+    #[STEP 3] : POST TO JSON
+    resDict = dict()
+    resDict['title'] = keyword
+    resDict['item_list'] = []
+    for product in product_list:
+        curDict = dict()
+        curDict = product
+        resDict['item_list'].append(curDict)
+
+    # SAVE JSON
+    cur_time = datetime.datetime.utcnow().strftime('%Y%m%d')
+    save_time = f'{str(cur_time)}'
+
+    ## 여기
+    with open(f'{now_path}/product_posts/{keyword}-{save_time}@.json', 'w', encoding='UTF-8') as file:
+        file.write(json.dumps(resDict, ensure_ascii=False))
+
+if __name__ == "__main__":
+    main()
---
+++ coupangapi/coupang.py
@@ -0,0 +1,107 @@
+import hmac
+import hashlib
+import os
+import requests
+import json
+from time import gmtime, strftime
+import urllib
+import urllib.request
+
+now_path = os.path.dirname(os.path.realpath(__file__))
+# file = open(f'{now_path}/metadata/setting.json',
+#             'r', encoding='utf-8')
+# config_dict = json.load(file)
+
+ACCESS_KEY = "7f9f186f-ae6a-48fd-a3f2-2ec9372a7f06"
+SECRET_KEY = "797423a893d5281f5e1ef957821913813e40cfa5"
+
+
+def generateHmac(method, url, api_secret_key, api_access_key):
+    path, *query = url.split('?')
+    os.environ['TZ'] = 'GMT+0'
+    dt_datetime = strftime('%y%m%d', gmtime()) + 'T' + strftime('%H%M%S', gmtime()) + 'Z'  # GMT+0
+    msg = dt_datetime + method + path + (query[0] if query else '')
+    signature = hmac.new(bytes(api_secret_key, 'utf-8'), msg.encode('utf-8'), hashlib.sha256).hexdigest()
+
+    return 'CEA algorithm=HmacSHA256, access-key={}, signed-date={}, signature={}'.format(api_access_key, dt_datetime,
+                                                                                          signature)
+
+
+def get_cuplink(linkList):
+    subId = 'pangcollector'
+
+    REQUEST = dict()
+    REQUEST['coupangUrls'] = linkList
+    REQUEST['subId'] = subId
+
+    request_method = 'POST'
+    domain = 'https://api-gateway.coupang.com'
+    api_url = '/v2/providers/affiliate_open_api/apis/openapi/v1/deeplink'
+
+    authorization = generateHmac(request_method, api_url, SECRET_KEY, ACCESS_KEY)
+    coupang_url = '{}{}'.format(domain, api_url)
+    response = requests.request(method=request_method,
+                                url=coupang_url,
+                                headers={'Authorization': authorization, 'Content-Type': 'application/json'},
+                                data=json.dumps(REQUEST),
+                                timeout=20
+                                )
+    print(response)
+    print(response.text)
+    # 쿠팡 API 호출[IF rCode]
+    if (response.json()['rCode'] != '0'):
+        print('쿠팡 API 호출 오류[' + str(response.json()['rCode']) + ']')
+
+    # 쿠팡 API 호출[result_data]'
+    result_data = response.json()['data']
+    return result_data
+
+
+def cupang_search(categoryId, limit):
+    # 쿠팡 API 호출[url 설정]
+    subId = 'pangcollector'
+    request_method = 'GET'
+    domain = 'https://api-gateway.coupang.com'
+    api_url = '/v2/providers/affiliate_open_api/apis/openapi/v1/products/bestcategories/' + str(
+        categoryId) + '?limit=' + str(limit) + '&subId=' + str(subId)
+
+    # 쿠팡 API 호출[response]
+    authorization = generateHmac(request_method, api_url, SECRET_KEY, ACCESS_KEY)
+    coupang_url = '{}{}'.format(domain, api_url)
+    response = requests.request(method=request_method,
+                                url=coupang_url,
+                                headers={'Authorization': authorization, 'Content-Type': 'application/json'}
+                                )
+
+    # 쿠팡 API 호출[IF rCode]
+    if (response.json()['rCode'] != '0'):
+        print('쿠팡 API 호출 오류[' + str(response.json()['rCode']) + ']')
+
+    # 쿠팡 API 호출[result_data]'
+    result_data = response.json()['data']
+    return result_data
+
+
+def cupang_keyword_search(keyword, limit):
+    # 쿠팡 API 호출[url 설정]
+    subId = 'pangcollector'
+    request_method = 'GET'
+    domain = 'https://api-gateway.coupang.com'
+    api_url = '/v2/providers/affiliate_open_api/apis/openapi/products/search?keyword=' + urllib.parse.quote(
+        keyword) + '&limit=' + str(limit) + '&subId=' + str(subId)
+
+    # 쿠팡 API 호출[response]
+    authorization = generateHmac(request_method, api_url, SECRET_KEY, ACCESS_KEY)
+    coupang_url = '{}{}'.format(domain, api_url)
+    response = requests.request(method=request_method,
+                                url=coupang_url,
+                                headers={'Authorization': authorization, 'Content-Type': 'application/json'}
+                                )
+
+    # 쿠팡 API 호출[IF rCode]
+    if (response.json()['rCode'] != '0'):
+        print('쿠팡 API 호출 오류[' + str(response.json()['rCode']) + ']')
+
+    # 쿠팡 API 호출[result_data]'
+    result_data = response.json()['data']['productData']
+    return result_data
